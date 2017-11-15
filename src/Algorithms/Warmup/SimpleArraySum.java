26f85516306852a7669295d607c4db5c091356f5
Animesh Nighojkar <animesh.nighojkar@gmail.com>
2017-11-15 08:04:24 +0530

    2. Simple Array Sum

--- src/Algorithms/Warmup/SimpleArraySum.java
+++ src/Algorithms/Warmup/SimpleArraySum.java
@@ -1,3 +1,4 @@
+//https://www.hackerrank.com/challenges/simple-array-sum/problem
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
