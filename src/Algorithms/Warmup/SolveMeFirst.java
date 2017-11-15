e324803333a6b7799a60586a38532ae298ee81b3
Animesh Nighojkar <animesh.nighojkar@gmail.com>
2017-11-15 07:36:07 +0530

    1. Solve Me First

---
+++ src/Algorithms/Warmup/SolveMeFirst.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithms.Warmup;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dumbledore
+ */
+public class SolveMeFirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a + b);
+    }
+
+}
